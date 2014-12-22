34478876364fc595131b85781691a6b725c93585
M.J. Hoag <mj.hoag@bettrlife.com>
2014-12-22 13:38:35 -0600

    Add support for $weakarray[] = $foo; (append assignment)

--- t/WeakArrayTest.php
+++ t/WeakArrayTest.php
@@ -26,4 +26,12 @@ class WeakArrayTest extends PHPUnit_Framework_TestCase {
 		unset($o);
 		$this->assertFalse(isset($a['o']));
 	}
+
+	public function testAppend() {
+		$o = new StdClass();
+		$a = new WeakArray();
+		$a[] = $o;
+		$a[] = $o;
+		$this->assertEquals($a[0], $a[1]);
+	}
 }
